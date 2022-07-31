69c8e1a2ec6b90839390e3ac530e201d0c8bcee9
u-aikay <uchey.ikenna@gmail.com>
2022-08-01 00:21:54 +0100

    initial commit

---
+++ src/routes/route.js
@@ -0,0 +1,19 @@
+
+import {
+  BrowserRouter as Router,
+  Routes as RouterCover,
+  Route,
+} from "react-router-dom";
+
+
+export const Routes = () => {
+  return (
+    <Router>
+        <RouterCover>
+            {/* <Route path="/" element={<Home />} /> */}
+        </RouterCover>
+    </Router>
+  );
+};
+
+export default Routes;
