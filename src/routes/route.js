59517a10441f87aa16ecf2cdfd467b5e2564228b
u-aikay <uchey.ikenna@gmail.com>
2022-08-01 22:00:35 +0100

    homepage navbar created

--- src/routes/route.js
+++ src/routes/route.js
@@ -4,13 +4,14 @@ import {
   Routes as RouterCover,
   Route,
 } from "react-router-dom";
+import Navbar from "../components/Navbar/navbar";
 
 
 export const Routes = () => {
   return (
     <Router>
         <RouterCover>
-            {/* <Route path="/" element={<Home />} /> */}
+            <Route path="/" element={<Navbar />} />
         </RouterCover>
     </Router>
   );
